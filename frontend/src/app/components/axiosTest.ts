7203e1458e46896adea26c0a3346887bb35664d2
Rocky Angelo Castañeros <rpcastaneros@up.edu.ph>
2025-05-11 20:56:37 +0800

    Delete frontend directory

--- frontend/src/app/components/axiosTest.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getMessage = async () => {
-  const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/`);
-  console.log(res.data);
-  return res.data;
-};
